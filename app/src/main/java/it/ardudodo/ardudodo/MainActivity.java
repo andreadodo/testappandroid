3698ad0f1453ce4d4ec83094fc6fd9529c4e495f
Andrea <andrea@AirDiAndrea.local>
2017-01-25 11:29:48 +0100

    project init

---
+++ app/src/main/java/it/ardudodo/ardudodo/MainActivity.java
@@ -0,0 +1,17 @@
+package it.ardudodo.ardudodo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by andrea on 25/01/17.
+ */
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
